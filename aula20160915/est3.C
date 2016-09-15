e0363a61933df433a18f4fa10cac6a6ad7f4743d
Your Name <you@example.com>
2016-09-15 12:19:23 -0300

    fim

---
+++ aula20160915/est3.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include QTDEMAX 1000
+struct CAD {
+  int idade;
+};
+int main(){
+struct CAD cadastro(QTDEMAX);
+int opc, i, qtde = 0;
+while(1){
+    printf("1 - cadastra  2 - sair")
+    scanf("%d", &opc);
+    if (opc == 2) break;
+    cadastro[qtde].idade = 3;
+    qtde++;
+}
+for()
+return 0;
+}
